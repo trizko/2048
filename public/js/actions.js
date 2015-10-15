dd9d7567532b05ec32fd2385bf9c74c15aa85770
Danny Rizko <danny@rizkocircle.com>
2015-10-14 23:33:27 -0700

    Add dynamic amount of divs based on column and row size Add form in HTML for user input

--- public/js/actions.js
+++ public/js/actions.js
@@ -1,9 +1,22 @@
-$(document).on('ready', function () {
+$(document).ready(function () {
 
-    window.game = new Game(4, 4);
+    var columnForm = $("#columnSize").val();
+    var rowForm = $("#rowSize").val();
+    console.log(columnForm, rowForm)
+    window.game = new Game(columnForm, rowForm);
+    var x = 1;
+    while (x <= game.rowLength * game.colLength) {
+        for (var i = 0; i < game.rowLength; i++) {
+            $('.table1').append("<tr class=\"row" + i + "\"></tr>")
+            for (var j = 0; j < game.colLength; j++) {
+                $('.row' + i).append("<td class=\"data" + x + "\"></td>")
+                x += 1
+            }
+        }
+    }
 
-    $("body").keyup(function (event) {
-        switch (event.which) {
+    $(document).keydown(function (key) {
+        switch (parseInt(key.which, 10)) {
             case 37:
                 game.moveLeft()
                 console.log(game.toString());
@@ -22,6 +35,19 @@ $(document).on('ready', function () {
                 break;
             default:
                 break;
+        }
+        var x = 1;
+        while (x <= game.rowLength * game.colLength) {
+            for (var i = 0; i < game.rowLength; i++) {
+                for (var j = 0; j < game.colLength; j++) {
+                    if (game.board[i][j] === 0) {
+                        $('.data' + x).html(" ")
+                    } else {
+                        $('.data' + x).html(game.board[i][j])
+                    }
+                    x += 1
+                }
+            }
         }
     });
 });
