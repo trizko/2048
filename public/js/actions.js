9d19ff99be99a483399a33685aa7631aee26974e
Danny Rizko <danny@rizkocircle.com>
2015-10-16 00:50:42 -0700

    Improve time complexity

--- public/js/actions.js
+++ public/js/actions.js
@@ -46,32 +46,26 @@ var newGame = function (columnForm, rowForm) {
 }
 
 var refreshData = function () {
-    for (var x = 1; x <= game.rowLength * game.colLength;) {
-        for (var i = 0; i < game.rowLength; i++) {
-            for (var j = 0; j < game.colLength; j++) {
-                if (game.board[i][j] === 0) {
-                    $('.data' + x).html(" ")
-                    $('.data' + x).css('background-color', '#E6CCCC')
-                    $('.data' + x).css('opacity', '0.4')
-                } else {
-                    $('.data' + x).html(game.board[i][j])
-                    $('.data' + x).css('background-color', '#18EC9E')
-                    $('.data' + x).css('opacity', '1.0')
-                }
-                x += 1
+    for (var i = 0; i < game.rowLength; i++) {
+        for (var j = 0; j < game.colLength; j++) {
+            if (game.board[i][j] === 0) {
+                $('.data' + i + j).html(" ")
+                $('.data' + i + j).css('background-color', '#E6CCCC')
+                $('.data' + i + j).css('opacity', '0.4')
+            } else {
+                $('.data' + i + j).html(game.board[i][j])
+                $('.data' + i + j).css('background-color', '#18EC9E')
+                $('.data' + i + j).css('opacity', '1.0')
             }
         }
     }
 }
 
 var appendData = function () {
-    for (var x = 1; x <= game.rowLength * game.colLength;) {
-        for (var i = 0; i < game.rowLength; i++) {
-            $('.table1').append("<tr class=\"row" + i + "\"></tr>")
-            for (var j = 0; j < game.colLength; j++) {
-                $('.row' + i).append("<td class=\"data" + x + "\"></td>")
-                x += 1
-            }
+    for (var i = 0; i < game.rowLength; i++) {
+        $('.table1').append("<tr class=\"row" + i + "\"></tr>")
+        for (var j = 0; j < game.colLength; j++) {
+            $('.row' + i).append("<td class=\"data" + i + j + "\"></td>")
         }
     }
 }
