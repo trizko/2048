9b98a9c6704e7bec5e069300167d983dbbe1e36f
Danny Rizko <danny@rizkocircle.com>
2015-10-21 22:47:30 -0700

    Change style layout from master

--- public/js/actions.js
+++ public/js/actions.js
@@ -19,21 +19,45 @@ $(document).ready(function () {
     $(document).keydown(function (key) {
         switch (parseInt(key.which, 10)) {
             case 37:
+                game.moveablesLeft.forEach(function (item) {
+                    TweenMax.to('#data' + item.join(''), .2, {
+                        x: -115,
+                        ease: Power4.easeInOut
+                    });
+                })
                 game.moveLeft()
                 break;
             case 38:
+                game.moveablesUp.forEach(function (item) {
+                    TweenMax.to('#data' + item.join(''), .2, {
+                        y: -115,
+                        ease: Power4.easeInOut
+                    });
+                })
                 game.moveUp()
                 break;
             case 39:
+                game.moveablesRight.forEach(function (item) {
+                    TweenMax.to('#data' + item.join(''), .2, {
+                        x: 115,
+                        ease: Power4.easeInOut
+                    });
+                })
                 game.moveRight()
                 break;
             case 40:
+                game.moveablesDown.forEach(function (item) {
+                    TweenMax.to('#data' + item.join(''), .2, {
+                        y: 115,
+                        ease: Power4.easeInOut
+                    });
+                })
                 game.moveDown()
                 break;
             default:
                 break;
         }
-        refreshData();
+        refreshData()
     });
 });
 
@@ -64,23 +88,29 @@ var newGame = function (columnForm, rowForm) {
 
 var appendData = function () {
     for (var i = 0; i < game.rowLength; i++) {
-        $('.table1').append("<tr class=\"row" + i + "\"></tr>")
+        $('body').append("<div class=\"row" + i + "\"></div>")
+        $('body').append("<div class=\"blankRow" + i + "\"></div>")
         for (var j = 0; j < game.colLength; j++) {
-            $('.row' + i).append("<td class=\"data" + i + j + "\"></td>")
+            if (game.board[i][j] !== 0) {
+                $('.row' + i).append("<div class=\"block\" id=\"data" + i + j + "\"></div>")
+                $('.blankRow' + i).append("<div class=\"blank\" id=\"data1" + i + j + "\"></div>")
+                $('#data' + i + j).css('top', (i + 1) * 115).css('left', (j + 1) * 115)
+                $('#data1' + i + j).css('top', (i + 1) * 115).css('left', (j + 1) * 115)
+            }
         }
     }
 }
 
-var refreshData = function () {
+var refreshData = function (direction) {
     for (var i = 0; i < game.rowLength; i++) {
         for (var j = 0; j < game.colLength; j++) {
             if (game.board[i][j] === 0) {
-                $('.data' + i + j).html(" ");
+                $('#data' + i + j).html(" ");
             } else {
-                $('.data' + i + j).html(game.board[i][j]);
-                $('.data' + i + j).css('opacity', '1.0');
+                $('#data' + i + j).html(game.board[i][j]);
+                $('#data' + i + j).css('opacity', '1.0');
             }
-            $('.data' + i + j).css('background-color', colors[game.board[i][j]]);
+            $('#data' + i + j).css('background-color', colors[game.board[i][j]]);
         }
     }
 }
