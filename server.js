a419c43f4a0f10e864773c8fb872fab2ca74b2bb
Danny Rizko <drizko07@gmail.com>
2016-03-05 20:31:45 -0800

    Adds node/express server

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
